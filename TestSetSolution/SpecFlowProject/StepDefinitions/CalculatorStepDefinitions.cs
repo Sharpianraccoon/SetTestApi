607fae35e62410b386a7ff87b53ee04e105ec738
Ian Sharp <Ian.Sharp@dominos.co.uk>
2023-08-08 14:46:13 +0100

    Project creation

---
+++ TestSetSolution/SpecFlowProject/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,30 @@
+namespace SpecFlowProject.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int firstNumber)
+        {
+            //todo
+        }
+
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int secondNumber)
+        {
+            //todo
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //todo
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int expectedNumber)
+        {
+            //todo
+        }
+    }
+}
---
+++ TestSetSolution/TestSetProject/features/TestSet.feature
@@ -0,0 +1,8 @@
+﻿Feature: TestSet
+
+A short summary of the feature
+
+Scenario: Test scenario
+	Given I have a persons name
+	When I request average age information for this name
+	Then the expected age is returned
